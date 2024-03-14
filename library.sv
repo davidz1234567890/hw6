01c286b00484395b653c58b475b57a2579c5dd5a
davidz1234567890 <129808533+davidz1234567890@users.noreply.github.com>
2024-03-14 15:24:53 -0400

    Update library.sv, all funcs

--- library.sv
+++ library.sv
@@ -1,6 +1,5 @@
 `default_nettype none
 
-
 module Decoder
     #(parameter WIDTH = 16)
     (
@@ -27,7 +26,6 @@ module Decoder
         end
       end
     endmodule: Decoder
-
 
     module BarrelShifter
     #(parameter WIDTH = 8)
@@ -58,7 +56,7 @@ module Decoder
       end
     endmodule: BarrelShifter
 
-   module Multiplexer
+    module Multiplexer
     #(parameter WIDTH = 32)
     (input logic [WIDTH-1:0] I,
     input logic [$clog2(WIDTH)-1:0] S,
@@ -78,7 +76,6 @@ module Decoder
         assign Y = I[S];
       end
     endmodule: Multiplexer
-
 
     module Mux2to1
         #(parameter WIDTH = 3)
@@ -140,3 +137,131 @@ module Decoder
     input logic bin, output logic bout, output logic [WIDTH-1:0] diff);
       assign {bout, diff} = A-B-bin;
     endmodule: Subtracter
+
+
+
+
+
+  module DFlipFlop
+    (input logic D, clock, reset_L, preset_L,
+      output logic Q);
+    always_ff @(posedge clock, negedge reset_L, negedge preset_L)
+    if (~reset_L & ~preset_L)
+      Q <= 1'bX;
+    
+    else if (~reset_L & preset_L)
+        Q <= 0;
+    else if (reset_L & ~preset_L)
+        Q <= 1;
+    else
+        Q <= D;
+    endmodule: DFlipFlop
+
+  //comment made here
+
+    module Register
+        #(parameter WIDTH=32)
+        (input logic en, clear,
+         input logic [WIDTH-1:0] D,
+          input logic clock,
+         output logic [WIDTH-1:0] Q);
+         
+        always_ff @(posedge clock)
+          if (en)
+            Q <= D;
+          else if (clear)
+            Q <= '0;
+            
+      endmodule : Register
+
+
+
+      module Counter
+        #(parameter WIDTH=32)
+        (input logic en, clear, load, up,
+         input logic [WIDTH-1:0] D,
+         input  logic clock,
+         output logic [WIDTH-1:0] Q);
+         
+        always_ff @(posedge clock)
+          if (clear)
+            Q <= '0;
+          else if (load)
+            Q <= D;
+          else if (en & up)
+            Q <= Q + 1;
+        // Else if en & ~up, count down
+          else if (en & ~up)
+            Q <= Q - 1;
+              
+      endmodule : Counter
+
+
+
+      module Synchronizer
+        (input logic async, clock,
+         output logic sync);
+        logic preset_L, reset_L;
+        logic ff_buf;
+        // Synchronize the signal by running it through two flip-flops (buffering)
+        
+        assign preset_L = 1'b1; //not asserted
+        assign reset_L = 1'b1; //not asserted
+        DFlipFlop dut1(.D(async),.Q(ff_buf), .clock,.preset_L,.reset_L);
+        DFlipFlop dut2(.D(ff_buf),.Q(sync),.clock,.preset_L, .reset_L);
+    
+    endmodule: Synchronizer
+
+
+
+    module ShiftRegister_SIPO
+        #(parameter WIDTH=32)
+        (input  logic en, left, serial,clock,
+         output logic [WIDTH-1:0] Q);
+         
+        always_ff @(posedge clock)
+          if (en & left)
+            Q <= {Q[WIDTH-2:0], serial};
+          else if(en & ~left)
+            Q <= {serial, Q[WIDTH-1:1]};
+              
+      endmodule : ShiftRegister_SIPO
+
+
+
+      module ShiftRegister_PIPO
+        #(parameter WIDTH=16)
+        (input logic en, left, load, 
+        input  logic [WIDTH-1:0] D,
+        input logic clock,
+         output logic [WIDTH-1:0] Q);
+         
+        always_ff @(posedge clock)
+          if (load)
+            Q <= D;
+          else if (en & left)
+            Q <= {Q[WIDTH-2:0], 1'b0};
+          else if (en & ~left)
+            Q <= {1'b0, Q[WIDTH-1:1]};
+              
+      endmodule : ShiftRegister_PIPO
+
+
+
+      module BarrelShiftRegister
+        #(parameter WIDTH=32)
+        (input logic en, load,
+        input logic [1:0] by,
+         input logic [WIDTH-1:0] D,
+         input  logic clock,
+         output logic [WIDTH-1:0] Q);
+         
+        
+        always_ff @(posedge clock)
+          if (load)
+              Q <= D;
+          else if (en)
+              Q <= (Q << by);
+          
+      endmodule : BarrelShiftRegister
+      
